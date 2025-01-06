dbd2a7e0abd1984cb65284d19cf67e8bba5a3724
Pedro Igor <peuigor09@gmail.com>
2025-01-05 21:38:44 -0300

    Adicionar logs adicionais e melhorias no script de remoção de fundo

--- u2net_test.py
+++ u2net_test.py
@@ -41,65 +41,74 @@ def save_output(image_name, pred, d_dir):
         imidx = imidx + "." + bbb[i]
 
     imo.save(os.path.join(d_dir, imidx + '.png'))
+    print(f"Imagem salva em: {os.path.join(d_dir, imidx + '.png')}")
 
 def main():
-    # --------- 1. get image path and name ---------
-    model_name = 'u2net'  # or 'u2netp'
+    try:
+        # --------- 1. get image path and name ---------
+        model_name = 'u2net'  # or 'u2netp'
+        print("Iniciando o script de remoção de fundo")
 
-    image_dir = os.path.join(os.getcwd(), 'test_data', 'test_images')
-    prediction_dir = os.path.join(os.getcwd(), 'test_data', model_name + '_results' + os.sep)
-    model_dir = os.path.join(os.getcwd(), 'saved_models', model_name, model_name + '.pth')
+        image_dir = os.path.join(os.getcwd(), 'test_data', 'test_images')
+        prediction_dir = os.path.join(os.getcwd(), 'test_data', model_name + '_results' + os.sep)
+        model_dir = os.path.join(os.getcwd(), 'saved_models', model_name, model_name + '.pth')
 
-    img_name_list = glob.glob(image_dir + os.sep + '*')
-    print(img_name_list)
+        img_name_list = glob.glob(image_dir + os.sep + '*')
+        print(f"Lista de imagens: {img_name_list}")
 
-    # --------- 2. dataloader ---------
-    test_salobj_dataset = SalObjDataset(
-        img_name_list=img_name_list,
-        lbl_name_list=[],
-        transform=transforms.Compose([RescaleT(320), ToTensorLab(flag=0)])
-    )
-    test_salobj_dataloader = DataLoader(test_salobj_dataset, batch_size=1, shuffle=False, num_workers=1)
+        # --------- 2. dataloader ---------
+        test_salobj_dataset = SalObjDataset(
+            img_name_list=img_name_list,
+            lbl_name_list=[],
+            transform=transforms.Compose([RescaleT(320), ToTensorLab(flag=0)])
+        )
+        test_salobj_dataloader = DataLoader(test_salobj_dataset, batch_size=1, shuffle=False, num_workers=1)
+        print("DataLoader configurado")
 
-    # --------- 3. model define ---------
-    if model_name == 'u2net':
-        print("...load U2NET---173.6 MB")
-        net = U2NET(3, 1)
-    elif model_name == 'u2netp':
-        print("...load U2NEP---4.7 MB")
-        net = U2NETP(3, 1)
-
-    if torch.cuda.is_available():
-        net.load_state_dict(torch.load(model_dir))
-        net.cuda()
-    else:
-        net.load_state_dict(torch.load(model_dir, map_location='cpu'))
-    net.eval()
-
-    # --------- 4. inference for each image ---------
-    for i_test, data_test in enumerate(test_salobj_dataloader):
-        print("Inferencing:", img_name_list[i_test].split(os.sep)[-1])
-
-        inputs_test = data_test['image']
-        inputs_test = inputs_test.type(torch.FloatTensor)
+        # --------- 3. model define ---------
+        if model_name == 'u2net':
+            print("...load U2NET---173.6 MB")
+            net = U2NET(3, 1)
+        elif model_name == 'u2netp':
+            print("...load U2NEP---4.7 MB")
+            net = U2NETP(3, 1)
 
         if torch.cuda.is_available():
-            inputs_test = Variable(inputs_test.cuda())
+            net.load_state_dict(torch.load(model_dir))
+            net.cuda()
         else:
-            inputs_test = Variable(inputs_test)
+            net.load_state_dict(torch.load(model_dir, map_location='cpu'))
+        net.eval()
+        print("Modelo carregado e configurado")
 
-        d1, d2, d3, d4, d5, d6, d7 = net(inputs_test)
+        # --------- 4. inference for each image ---------
+        for i_test, data_test in enumerate(test_salobj_dataloader):
+            print(f"Inferenciando: {img_name_list[i_test].split(os.sep)[-1]}")
 
-        # normalization
-        pred = d1[:, 0, :, :]
-        pred = normPRED(pred)
+            inputs_test = data_test['image']
+            inputs_test = inputs_test.type(torch.FloatTensor)
 
-        # save results to test_results folder
-        if not os.path.exists(prediction_dir):
-            os.makedirs(prediction_dir, exist_ok=True)
-        save_output(img_name_list[i_test], pred, prediction_dir)
+            if torch.cuda.is_available():
+                inputs_test = Variable(inputs_test.cuda())
+            else:
+                inputs_test = Variable(inputs_test)
 
-        del d1, d2, d3, d4, d5, d6, d7
+            d1, d2, d3, d4, d5, d6, d7 = net(inputs_test)
+
+            # normalization
+            pred = d1[:, 0, :, :]
+            pred = normPRED(pred)
+
+            # save results to test_results folder
+            if not os.path.exists(prediction_dir):
+                os.makedirs(prediction_dir, exist_ok=True)
+            save_output(img_name_list[i_test], pred, prediction_dir)
+
+            del d1, d2, d3, d4, d5, d6, d7
+
+        print("Processamento concluído com sucesso")
+    except Exception as e:
+        print(f"Erro no processamento da imagem: {str(e)}")
 
 if __name__ == "__main__":
     main()
